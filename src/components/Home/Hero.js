0ad8beac6ace75984465469852e19d15363790b2
Md. Safwan <sayedmdsafwan@gmail.com>
2022-05-28 15:15:00 +0600

    added resume successfully

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -8,13 +8,25 @@ const Hero = () => {
         >
             <div className="hero-content text-center">
                 <div className="max-w-md lg:max-w-3xl">
+                    <h3 className="text-2xl font-light text-primary mb-4">
+                        Hi, this is Safwan
+                    </h3>
                     <h1 className="lg:text-5xl text-3xl mb-8 tracking-wider font-extralight">
                         I love to visualize ideas and make them live into
                         websites
                     </h1>
                     <a href="#contact">
-                        <button className="btn btn-outline btn-primary">
+                        <button className="btn btn-outline mr-2 btn-primary">
                             Contact Me
+                        </button>
+                    </a>
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://drive.google.com/file/d/1v01-IJJ272vvBxBih7YxjU4LdvyWuICt/view?usp=sharing"
+                    >
+                        <button className="btn btn-outline ml-2 btn-secondary">
+                            Download CV
                         </button>
                     </a>
                 </div>
